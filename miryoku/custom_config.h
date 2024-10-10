d1a8dd09071eb2045c200ccf47b5ca26c6a92efa
Edward Martinez <edwardmartinez00@gmail.com>
2024-10-10 02:56:32 -0700

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+//custom macro
+U_MACRO(U_CFN, bindings = <&kp F2 &kp LA(A)>;)
+
 #define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH_FLIP \
 U_NU,             &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,              U_NU,             \
 U_NU,             U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),        U_NU,             \
